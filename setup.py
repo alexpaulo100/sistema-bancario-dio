958d0336735d94a80c45f2e6f31ed1e0c0a536cb
Alex Silva <alex.paulo100@gmail.com>
2024-08-23 16:46:24 -0300

    configure the initial project organization

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="bcdio",
+    version="0.1.0",
+    description="Sistema Bancario",
+    author="Alex Silva",
+    packages=find_packages(),
+
+)
